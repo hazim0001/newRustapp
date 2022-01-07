c07ee5f31a3c9cfe5ee2a5c5c8b0275f6d79062b
hazim0001 <hazim_alsayed@hotmail.com>
2022-01-07 23:49:37 +0200

    built a program that complies ruby file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+// use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+  // let mut cmd = Command::new("ruby");
+
+  // cmd.arg("ruby.rb");
+
+  // match cmd.output() {
+  //   Ok(o) => unsafe {
+  //     println!("Output: {}", String::from_utf8_unchecked(o.stdout));
+  //   },
+  //   Err(e) => {
+  //     println!("There was an error {}", e);
+  //   }
+  // }
+
+  println!("Hello, world!");
+
+  let test = 90;
+  assert_eq!(test, 90);
+  println!("{}", test);
+
+  for i in 0..599999999 {
+    if i % 2 == 0 {
+      println!("even {}", i);
+    } else {
+      println!("odd {}", i);
+    }
+  }
 }
